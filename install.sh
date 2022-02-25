74382f9347e587f6a61d9ea7aa502a16cd314b35
lukas9393 <11976901+lukas9393@users.noreply.github.com>
2022-02-25 12:05:16 +0100

    wip

--- install.sh
+++ install.sh
@@ -4,3 +4,7 @@ ln -fs "${PWD}/.vimrc" "${HOME}/.vimrc"
 ln -fs "${PWD}/.gitignore" "${HOME}/.gitignore"
 ln -fs "${PWD}/.gitconfig" "${HOME}/.gitconfig"
 ln -fs "${PWD}/.gitattributes" "${HOME}/.gitattributes"
+
+mkdir -p .zsh
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
